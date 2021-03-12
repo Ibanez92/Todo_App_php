6f2d4adfeb51802beaa8c3b9a72b820d4a221912
Javier Ibanez <expertprep2015@gmail.com>
2021-03-12 00:28:30 -0500

    made the main title text bigger and changed color text to white. added the form and input/button in my welcome.blade.php file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.app')
 @section('content')
-    dfg
+    <div class="w-100 h-100 d-flex justify-content-center align-items-center">
+        <div class="w-50">
+        <h1 class="display-2 text-white">Todo APP</h1>
+        <form action="{{ route('todo.store') }}" method="POST">
+            @csrf
+            <div class="input-group mb-3 w-100">
+                <input type="text" class="form-control form-control-lg " name='title' placeholder="Type here.." aria-label="Recipient's username" aria-describedby="button-addon2">
+                <div class="input-group-append">
+                    <button class="btn btn-success" type="submit" id="button-addon2">Add to the list</button>
+                </div>
+            </div>
+        </form>
+    </div>
+        
+    </div>
 @endsection
